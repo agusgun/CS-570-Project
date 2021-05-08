fix normalization layer

main_training_script/utils/normalization_layer.py
@@ -31,7 +31,7 @@ class GNPlusParallel(nn.Module):
         self.gamma = nn.Parameter(torch.ones(c_num, 1, 1))
         self.beta = nn.Parameter(torch.zeros(c_num, 1, 1))
         self.eps = eps
-        self.lambda_param = nn.Parameter(torch.tensor([0.5]))
+        self.lambda_param = nn.Parameter(torch.ones(1))
 
     def forward(self, x):
         N, C, H, W = x.size()
@@ -46,7 +46,7 @@ class GNPlusParallel(nn.Module):
         std_bn = x.std(dim=0, keepdim=True)
         x = (x -  mean_bn) / (std_bn + self.eps)
         
-        return (self.lambda_param[0] * x_gn + (1 - self.lambda_param[0]) * x) * self.gamma + self.beta
+        return (torch.sigmoid(self.lambda_param[0]) * x_gn + (1 - torch.sigmoid(self.lambda_param[0])) * x) * self.gamma + self.beta
 
 # Our Proposed Solution
 class GNPlusSequentialGNFirst(nn.Module):
@@ -56,7 +56,7 @@ class GNPlusSequentialGNFirst(nn.Module):
         self.gamma = nn.Parameter(torch.ones(c_num, 1, 1))
         self.beta = nn.Parameter(torch.zeros(c_num, 1, 1))
         self.eps = eps
-        self.lambda_param = nn.Parameter(torch.tensor([0.5]))
+        self.lambda_param = nn.Parameter(torch.ones(1))
 
     def forward(self, x):
         N, C, H, W = x.size()
@@ -71,7 +71,7 @@ class GNPlusSequentialGNFirst(nn.Module):
         std_bn = x_gn.std(dim=0, keepdim=True)
         x_bn = (x_gn -  mean_bn) / (std_bn + self.eps)
         
-        return (self.lambda_param[0] * x_gn + (1 - self.lambda_param[0]) * x_bn) * self.gamma + self.beta
+        return (torch.sigmoid(self.lambda_param[0]) * x_gn + (1 - torch.sigmoid(self.lambda_param[0])) * x_bn) * self.gamma + self.beta
 
 # Our Proposed Solution
 class GNPLusSequentialBNFirst(nn.Module):
@@ -96,7 +96,7 @@ class GNPLusSequentialBNFirst(nn.Module):
         x_gn = (x_gn - mean_gn) / (std_gn + self.eps)
         x_gn = x_gn.view(N, C, H, W)
 
-        return (self.lambda_param[0] * x_gn + (1 - self.lambda_param[0]) * x_bn) * self.gamma + self.beta
+        return (torch.sigmoid(self.lambda_param[0]) * x_gn + (1 - torch.sigmoid(self.lambda_param[0])) * x_bn) * self.gamma + self.beta
 
 def get_norm_layer(c_out, n_group=32, norm='bn'):
     if norm == 'bn':